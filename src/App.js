5b25bfca3aab7e7526907155b3aca4332be2c3b7
EDMOND G <gihozoedmond@gmail.com>
2021-04-28 16:35:41 +0200

    arrange the basic dashboard layouts and authentication pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./components/secure/tools/Dashboard";
+import Users from "./components/secure/tools/Users";
+// public components
+import Login from "./components/auth/login";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<Route path={"/"} component={Dashboard} exact />
+				<Route path={"/users"} component={Users} />
+				<Route path={"/login"} component={Login} />
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/secure/tools/Wrapper.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Nav from "../layouts/Nav";
+import TopBar from "../layouts/TopBar";
+import logo from "../../../images/logo.svg";
+
+class Wrapper extends Component {
+	render() {
+		return (
+			<>
+				<main className="bg-gray-100 dark:bg-gray-800 rounded-2xl relative h-screen overflow-hidden relative">
+					<div className="flex items-start justify-between">
+						<div className="h-screen hidden lg:block my-4 ml-4 shadow-lg relative w-80">
+							<div className="bg-white h-full rounded-2xl dark:bg-gray-700">
+								<div className="flex items-center justify-center pt-6">
+									<img src={logo} alt="logo" className="w-28 h-28" />
+								</div>
+								<Nav />
+							</div>
+						</div>
+						<div className="flex flex-col w-full pl-0 md:p-4 md:space-y-4">
+							<TopBar />
+						</div>
+					</div>
+				</main>
+			</>
+		);
+	}
+}
+
+export default Wrapper;
